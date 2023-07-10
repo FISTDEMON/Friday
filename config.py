bc1e84324fab1438b36329297dde7fb42de5d31d
A/R <112615664+FISTDEMON@users.noreply.github.com>
2023-07-10 12:27:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+#todo: add apikey here
+
+apikey = "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"
+
+#"C:\\Users\\anish\\OneDrive\\Desktop\\browser_assistant\\chromedriver.exe"
+#"C:\\ProgramData\\chocolatey\\lib\\chromedriver\\tools"
+#//*[@id="APjFqb"]
